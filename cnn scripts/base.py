0ed0ebcea68eef4270c025d10e488ecf05bf83be
anthonylim0 <98455116+anthonylim0@users.noreply.github.com>
2025-03-17 15:48:35 +0800

    inference using vanilla resnet50

---
+++ cnn scripts/base.py
@@ -0,0 +1,10 @@
+# import pandas as pd
+# from datasets import Dataset, Image
+
+# df_train = pd.read_csv('../data/melspectrogram_train_dataset.csv')
+
+# print(df_train)
+
+import torch
+print(torch.__version__)
+print(torch.cuda.is_available())
