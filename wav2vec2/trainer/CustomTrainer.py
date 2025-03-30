2399fadda0104bcd91de51afd34a1706dd996acd
anthonylim0 <98455116+anthonylim0@users.noreply.github.com>
2025-03-30 20:37:41 +0800

    Finetune large finetuned wav2vec2 on english

--- wav2vec2/trainer/CustomTrainer.py
+++ wav2vec2/trainer/CustomTrainer.py
@@ -5,17 +5,16 @@ from transformers import Trainer
 class CustomTrainer(Trainer):
     def __init__(self, *args, class_weights_path, **kwargs):
         super().__init__(*args, **kwargs)
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
         # Load class weights
-        self.class_weights = torch.load(class_weights_path).to(device)
+        self.class_weights = torch.load(class_weights_path).to(self.device)
         
         # Custom loss function: Cross entropy loss weighted
         self.criterion = nn.CrossEntropyLoss(weight=self.class_weights)
         
     def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        labels = inputs.pop("labels").to(device)
+        labels = inputs.pop("labels").to(self.device)
         outputs = model(**inputs)
         logits = outputs.logits
         loss = self.criterion(logits, labels)
