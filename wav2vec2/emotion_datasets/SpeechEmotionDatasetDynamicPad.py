178c16de064c959458ed649830bccc8d0578484f
anthonylim0 <anthony.rclim@gmail.com>
2025-04-04 02:14:03 +0800

    added early stopping and whisper script

--- wav2vec2/emotion_datasets/SpeechEmotionDatasetDynamicPad.py
+++ wav2vec2/emotion_datasets/SpeechEmotionDatasetDynamicPad.py
@@ -3,13 +3,11 @@ import librosa
 import torch
 from torch.utils.data import Dataset
 
-# Custom dataset class, pads audios to 4s long
-class SpeechEmotionDatasetPadding(Dataset):
-    # Max_length = 4s, 64000 because sampling rate is 16000
-    def __init__(self, df, processor, max_length=64000):
+# Custom dataset class, pad to maximum length in the training batch
+class SpeechEmotionDatasetDynamicPad(Dataset):
+    def __init__(self, df, processor):
         self.df = df
         self.processor = processor
-        self.max_length = max_length
 
     def __len__(self):
         return len(self.df)
@@ -21,13 +19,8 @@ class SpeechEmotionDatasetPadding(Dataset):
         # Load audio file
         speech, sr = librosa.load(audio_path, sr=16000)
 
-        # Pad speech to required length
-        speech = np.pad(speech, (0, self.max_length -
-                        len(speech)), mode='constant')
-
         # Preprocess audio
-        inputs = self.processor(speech, sampling_rate=16000, return_tensors='pt',
-                                padding=True, truncate=True, max_length=self.max_length)
+        inputs = self.processor(speech, sampling_rate=16000, return_tensors='pt')
 
         input_values = inputs.input_values.squeeze()
         return {'input_values': input_values, 'labels': torch.tensor(label, dtype=torch.long)}
--- wav2vec2/emotion_datasets/SpeechEmotionDatasetStandardPad.py
+++ wav2vec2/emotion_datasets/SpeechEmotionDatasetStandardPad.py
@@ -3,11 +3,13 @@ import librosa
 import torch
 from torch.utils.data import Dataset
 
-# Custom dataset class, np padding
-class SpeechEmotionDataset(Dataset):
-    def __init__(self, df, processor):
+# Custom dataset class, standard padding of audios to 4s long
+class SpeechEmotionDatasetStandardPad(Dataset):
+    # Max_length = 4s, 64000 because sampling rate is 16000
+    def __init__(self, df, processor, max_length=64000):
         self.df = df
         self.processor = processor
+        self.max_length = max_length
 
     def __len__(self):
         return len(self.df)
@@ -19,8 +21,13 @@ class SpeechEmotionDataset(Dataset):
         # Load audio file
         speech, sr = librosa.load(audio_path, sr=16000)
 
+        # Pad speech to required length
+        speech = np.pad(speech, (0, self.max_length -
+                        len(speech)), mode='constant')
+
         # Preprocess audio
-        inputs = self.processor(speech, sampling_rate=16000, return_tensors='pt')
+        inputs = self.processor(speech, sampling_rate=16000, return_tensors='pt',
+                                padding=True, truncate=True, max_length=self.max_length)
 
         input_values = inputs.input_values.squeeze()
         return {'input_values': input_values, 'labels': torch.tensor(label, dtype=torch.long)}
