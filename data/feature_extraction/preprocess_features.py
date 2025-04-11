69c410a532d697564dc39bb147e961eb5ec6c1ff
Joel Tay <joeltwh1999@gmail.com>
2025-04-11 15:46:12 +0800

    final ffnn and eval

--- data/feature_extraction/preprocess_features.py
+++ data/feature_extraction/preprocess_features.py
@@ -1,14 +1,26 @@
 import pandas as pd
-from FeaturePreprocessor import FeaturePreprocessor
-preprocessor = FeaturePreprocessor(n_components_pca=0.99, n_features_select=50)
+from data.feature_extraction.FeaturePreprocessor import FeaturePreprocessor
+from sklearn.preprocessing import LabelEncoder
+from pathlib import Path
 
-def main():
-  df = pd.read_csv('../features/combined_features.csv', on_bad_lines='warn')
+current_file = Path(__file__).resolve()
+base_path = current_file.parents[1]
+
+label_encoder = LabelEncoder()
+
+speech_df = pd.read_csv(f'{base_path}/speech_dataset.csv', on_bad_lines='warn')
+
+def main(n_features_select=50):
+  preprocessor = FeaturePreprocessor(n_components_pca=0.99, n_features_select=n_features_select)
+  df = pd.read_csv(f'{base_path}/features/combined_features.csv', on_bad_lines='warn')
   full_df = df.set_index("Filepath")
 
-  train_set = pd.read_csv('../train_dataset.csv')
-  val_set = pd.read_csv('../val_dataset.csv')
-  test_set = pd.read_csv('../test_dataset.csv')
+  # get label class nums
+  label_encoder.fit(speech_df["Emotion"].unique())
+
+  train_set = pd.read_csv(f'{base_path}/train_dataset.csv')
+  val_set = pd.read_csv(f'{base_path}/val_dataset.csv')
+  test_set = pd.read_csv(f'{base_path}/test_dataset.csv')
 
   cols_to_drop = ['Id', 'Dataset', 'Filename', 'Ext', 'Duration']
 
@@ -17,7 +29,7 @@ def main():
   test_df = full_df.merge(test_set, on='Filepath', how='inner').drop(columns=cols_to_drop).set_index('Filepath')
 
   X_train = train_df.iloc[:, :-1]
-  y_train = train_df.iloc[:, -1]
+  y_train = label_encoder.transform(train_df.iloc[:, -1])
 
   print(f"Features shape: {X_train.shape}")
   print(f"Target shape: {y_train.shape}")
@@ -31,40 +43,37 @@ def main():
   # Normalize features
   X_train_final = preprocessor.normalize_pc(X_train_reduced)
 
-  # Plot feature importance
-  preprocessor.plot_feature_importance(X_train_final, y_train)
+  # # Plot feature importance
+  # preprocessor.plot_feature_importance(X_train_final, y_train)
 
-  # Plot PCA variance
-  preprocessor.plot_pca_variance()
+  # # Plot PCA variance
+  # preprocessor.plot_pca_variance()
 
-  train_final = X_train_final.join(y_train, how='inner')
+  # train_final = X_train_final.join(y_train, how='inner')
 
   ## process validation and test sets
   print("val")
   X_val = val_df.iloc[:, :-1]
-  y_val = val_df.iloc[:, -1]
+  y_val = label_encoder.transform(val_df.iloc[:, -1])
   X_val_normalized = preprocessor.normalize_features(X_val, fit=False)
   X_val_reduced = preprocessor.reduce_dimensions(X_val_normalized, y_val, fit=False)
   X_val_final = preprocessor.normalize_pc(X_val_reduced, fit=False)
-  val_final = X_val_final.join(y_val, how='inner')
 
   print("test")
   X_test = test_df.iloc[:, :-1]
-  y_test = test_df.iloc[:, -1]
+  y_test = label_encoder.transform(test_df.iloc[:, -1])
   X_test_normalized = preprocessor.normalize_features(X_test, fit=False)
   X_test_reduced = preprocessor.reduce_dimensions(X_test_normalized, y_test, fit=False)
   X_test_final = preprocessor.normalize_pc(X_test_reduced, fit=False)
-  test_final = X_test_final.join(y_test, how='inner')
-
-  print("final shapes:")
-  print("train: ", train_final.shape)
-  print("val: ", val_final.shape)
-  print("test: ", test_final.shape)
 
   return {
-    "train": train_final,
-    "val": val_final,
-    "test": test_final
+    "X_train": X_train_final,
+    "y_train": y_train,
+    "X_val": X_val_final,
+    "y_val": y_val,
+    "X_test": X_test_final,
+    "y_test": y_test,
+    "label_encoder": label_encoder # so can transform back to string later
   }
 
 if __name__ == '__main__':
---
+++ ffnn/hparams_tune.py
@@ -0,0 +1,126 @@
+import optuna
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
+from sklearn.utils.class_weight import compute_class_weight
+import numpy as np
+from data.feature_extraction import preprocess_features
+from ffnn.nn_classifier import NNClassifier
+from functools import partial
+
+# n_features_select = 200
+
+def prepare_data(num_features):
+    res = preprocess_features.main(num_features)
+    label_encoder = res['label_encoder']
+
+    X_train_tensor = torch.tensor(res["X_train"].values, dtype=torch.float32)
+    y_train_tensor = torch.tensor(res["y_train"], dtype=torch.int64)
+
+    X_val_tensor = torch.tensor(res["X_val"].values, dtype=torch.float32)
+    y_val_tensor = torch.tensor(res["y_val"], dtype=torch.int64)
+
+    X_test_tensor = torch.tensor(res["X_test"].values, dtype=torch.float32)
+    y_test_tensor = torch.tensor(res["y_test"], dtype=torch.int)
+
+    class_weights = compute_class_weight(
+        class_weight="balanced",
+        classes=np.unique(res["y_train"]),
+        y=res["y_train"]
+    )
+    class_weights_tensor = torch.tensor(class_weights, dtype=torch.float32)
+
+    train_ds = TensorDataset(X_train_tensor, y_train_tensor)
+    val_ds = TensorDataset(X_val_tensor, y_val_tensor)
+    test_ds = TensorDataset(X_test_tensor, y_test_tensor)
+
+    train_loader = DataLoader(train_ds, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_ds, batch_size=32)
+    test_loader = DataLoader(test_ds, batch_size=32)
+
+    return train_loader, val_loader, test_loader, len(label_encoder.classes_), class_weights_tensor, label_encoder
+
+def objective(trial):
+    # Tune feature count: 100 to 1000 in steps of 100
+    n_features_select = trial.suggest_int("n_features_select", 100, 1000, step=100)
+
+    # Prepare data based on number of selected features
+    train_loader, val_loader, _, num_classes, class_weights_tensor, _ = prepare_data(n_features_select)
+
+    # hidden_dims = []
+    # num_layers = trial.suggest_int("num_layers", 1, 2)
+    # for i in range(num_layers):
+    #     hidden_dims.append(trial.suggest_categorical(f"hidden_{i}", [2 ** x for x in range(4, 9)]))
+    # Tune hidden dimension from 16 to 256
+    hidden_dim = trial.suggest_categorical("hidden_dim", [2 ** x for x in range(5, 10)])
+
+    dropout = trial.suggest_float("dropout", 0.1, 0.5, step=0.1) # step of 0.1
+    lr = trial.suggest_categorical("lr", [0.00001, 0.0001, 0.001])
+
+    model = NNClassifier(
+        input_dim=n_features_select,
+        # hidden_dims=hidden_dims,
+        hidden_dims=[hidden_dim],
+        num_classes=num_classes,
+        dropout=dropout,
+        lr=lr,
+        class_weights=class_weights_tensor,
+    )
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_acc",
+        mode="max",
+        save_top_k=1,
+        dirpath="checkpoints",
+        filename=f"trial_{trial.number}_best",
+    )
+    early_stop_callback = EarlyStopping(
+        monitor="val_acc",
+        patience=5,
+        mode="max",
+        verbose=True
+    )
+
+    trainer = L.Trainer(
+        max_epochs=50,
+        accelerator="mps" if torch.backends.mps.is_available() else "auto",
+        logger=False,
+        callbacks=[checkpoint_callback, early_stop_callback]
+    )
+
+    trainer.fit(model, train_loader, val_loader)
+    val_result = trainer.validate(model, val_loader)[0]
+
+    trial.set_user_attr("checkpoint_path", checkpoint_callback.best_model_path)
+    trial.set_user_attr("n_features", n_features_select)
+    return val_result["val_acc"]
+
+
+if __name__ == "__main__":
+    # train_loader, val_loader, test_loader, num_classes, class_weights_tensor, label_encoder = prepare_data(n_features_select)
+
+    # objective_with_data = partial(
+    #     objective,
+    #     train_loader=train_loader,
+    #     val_loader=val_loader,
+    #     num_classes=num_classes,
+    #     class_weights_tensor=class_weights_tensor,
+    #     num_features=n_features_select
+    # )
+
+    # study = optuna.create_study(direction="maximize",
+    #                             study_name="ffnn",
+    #                             storage="sqlite:///optuna_study.db",
+    #                             load_if_exists=True)
+    # study.optimize(objective_with_data, n_trials=30)
+
+    study = optuna.create_study(direction="maximize",
+                                study_name="ffnn-n-features",
+                                storage="sqlite:///optuna_study.db",
+                                load_if_exists=True)
+    study.optimize(objective, n_trials=30)
+
+    print("Best trial:")
+    print(study.best_trial)
+
